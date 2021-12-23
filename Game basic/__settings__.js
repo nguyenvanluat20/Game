2db4f2ad6bc02d20a2b61c32f0d48da6587a9542
ADMIN\Admin <nguyenvanluat2k@gmail.com>
2021-12-23 13:00:10 +0700

    first commit

---
+++ Game basic/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1297960.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 63698450, 63698533, 63698536 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/63697219/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/63697220/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/63697218/1/ammo.js', 'preload' : true},
+];
